c23792e50a46d57b8cb537c1a6df71ceb34c8bd1
Charles Mesquita <charles_mesquita@kawasakibrasil.com>
2022-01-30 13:34:24 -0300

    Instalação e implementação do ngrx store e o dev tools com a simulação do redux devtools no chorme preparação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomePage } from './pages/home/home.page';
 
 const routes: Routes = [
   { path: '', component: HomePage},
-  { path: 'bookmarks', component: BookmarksPage}
+  { path: 'bookmarks', component: BookmarksPage},
 ];
 
 @NgModule({
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { StoreModule } from '@ngrx/store';
+
 import { HomePage } from './home.page';
+import { homeReducer } from './state/home.reducer';
 
 
 
 @NgModule({
   declarations: [HomePage],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+      StoreModule.forFeature('home', homeReducer),
   ]
 })
 export class HomeModule { }
