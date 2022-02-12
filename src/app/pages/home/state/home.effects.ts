beee4485c20f8944c508b3d05be598a5a964f672
Charles Mesquita <charles_mesquita@kawasakibrasil.com>
2022-02-12 18:52:03 -0300

    implementar effects

--- src/app/pages/home/state/home.effects.ts
+++ src/app/pages/home/state/home.effects.ts
@@ -8,6 +8,14 @@ import * as fromHomeActions from './home.actions';
 @Injectable()
 export class HomeEffects {
 
+  test$ = createEffect(()=> this.actions$
+      .pipe(
+        ofType(fromHomeActions.changeText),
+        map(({ text })=> console.log(text)),
+      ),
+      {dispatch: false},
+    );
+
   
 
   constructor(private actions$: Actions) {
