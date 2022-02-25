d1b60147fa1b9a3ffe5b3e1af3b9074cefcb6fe9
Charles Mesquita <charles_mesquita@kawasakibrasil.com>
2022-02-24 22:13:11 -0300

    implementação do detailed-weather com erro no jv-loader

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -8,7 +8,6 @@ import { EffectsModule } from '@ngrx/effects';
 import { HomePage } from './home.page';
 import { homeReducer } from './state/home.reducer';
 import { HomeEffects } from './state/home.effects';
-import { ComponentsModule } from 'src/app/shared/components/components.module';
 import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
 
 @NgModule({
@@ -17,7 +16,6 @@ import { CurrentWeatherComponent } from './components/current-weather/current-we
     ReactiveFormsModule,
     StoreModule.forFeature('home', homeReducer),
     EffectsModule.forFeature([HomeEffects]),
-    ComponentsModule,
   ],
   declarations: [
     HomePage,
