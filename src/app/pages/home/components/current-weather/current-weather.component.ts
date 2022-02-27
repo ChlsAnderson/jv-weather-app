1d9968face87d3415c7a949674698bacf7d78e1a
Charles Mesquita <charles_mesquita@kawasakibrasil.com>
2022-02-27 15:05:39 -0300

    correção do onToggleBookmark no template current-weather.component

--- src/app/pages/home/components/current-weather/current-weather.component.ts
+++ src/app/pages/home/components/current-weather/current-weather.component.ts
@@ -13,7 +13,7 @@ export class CurrentWeatherComponent {
   @Output() toggleBookmark = new EventEmitter();
 
   get cityName(): string {
-    return `${this.cityWeather.city.name} ${this.cityWeather.city.country}`
+    return `${this.cityWeather.city.name} ${this.cityWeather.city.country}`;
   }
 
   onToggleBookmark() {
